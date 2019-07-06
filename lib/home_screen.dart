18675a6dfe075a04f2203f90d290da64760deaf4
Gabriel Victor <dester.gvpdv@gmail.com>
2019-07-06 01:49:52 -0300

    finished app

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,11 +6,13 @@ import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance";
 
+// Asynchronous operation for get currency data
 Future<Map> getData() async {
   http.Response response = await http.get(request);
   return json.decode(response.body);
 }
 
+// Home Screen class with stateful widget
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
